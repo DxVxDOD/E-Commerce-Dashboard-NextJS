f649efb5a47a3acd716feeb65e58f8fbe1d82a01
DxVxDOD <orbangdavid@yahoo.com>
2023-11-07 21:19:07 +0200

    navbar finished

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,12 +1,28 @@
-import { UserButton } from "@clerk/nextjs";
+import { auth, UserButton } from "@clerk/nextjs";
 import MainNavbar from "@/components/nav/MainNavbar";
 import StoreSwitcher from "@/components/nav/StoreSwitcher";
+import { redirect } from "next/navigation";
+import { wrapInObject } from "@/lib/promiseWrap";
+import { Store } from ".prisma/client";
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const { userId } = auth();
+
+  if(!userId) redirect('/sign-in');
+
+  const { data: storesData, error: storesError } = await wrapInObject<Store[]>(prisma?.store.findMany({
+    where: {
+      userId
+    }
+  }))
+
+  if (storesError) console.log(storesError)
+
   return (
     <section className={"border-b border-black"}>
       <div className={"flex h-16 items-center px-4"}>
-        <StoreSwitcher/>
+        <StoreSwitcher items={storesData as Store[]} />
         <div>Routes</div>
         <MainNavbar />
         <div className={"ml-auto flex items-center space-x-4"}>
--- src/components/nav/StoreSwitcher.tsx
+++ src/components/nav/StoreSwitcher.tsx
@@ -3,13 +3,22 @@
 import { ComponentPropsWithoutRef, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Store } from ".prisma/client";
-import { useAppSelector } from "@/redux/hooks";
-import { selectModals } from "@/redux/features/modalSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import { onOpen } from "@/redux/features/modalSlice";
 import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Command, CommandInput, CommandList } from "@/components/ui/command";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator
+} from "@/components/ui/command";
+import { store } from "@/redux/store";
 
 type PopoverTriggerProps = ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -20,9 +29,9 @@ interface StoreSwitcherProps extends PopoverTriggerProps {
 const StoreSwitcher = ({className, items = []}: StoreSwitcherProps) => {
   const [open, setOpen] = useState(false);
 
-  const modal = useAppSelector(state => selectModals(state));
   const params = useParams();
   const router = useRouter();
+  const dispatch = useAppDispatch()
 
   const formattedItems = items.map(item => ({
     label: item.name,
@@ -48,7 +57,7 @@ const StoreSwitcher = ({className, items = []}: StoreSwitcherProps) => {
         className={cn('w-[200px] justify-between', className)}
         >
           <StoreIcon className={'mr-2 h-4 w-4'} />
-          Current Store
+          {currentStore?.label}
           <ChevronsUpDown className={'ml-auto h-4 w-4 shrink-0 opacity-50'} />
         </Button>
       </PopoverTrigger>
@@ -56,6 +65,41 @@ const StoreSwitcher = ({className, items = []}: StoreSwitcherProps) => {
         <Command>
           <CommandList>
             <CommandInput placeholder={'Search store....'} />
+            <CommandEmpty>
+              No store found.
+            </CommandEmpty>
+            <CommandGroup heading={'Stores'} >
+              {formattedItems.map(store => (
+                <CommandItem
+                key={store.value}
+                onSelect={() => onStoreSelect(store)}
+                className={'text-sm'}
+                >
+                  <StoreIcon/>
+                  {store.label}
+                  <Check
+                  className={cn('ml-auto h-4 w-4',
+                  currentStore?.value === store.value ?
+                  'opacity-100' :
+                  'opacity-0')}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator/>
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+              onSelect={() => {
+                setOpen(false);
+                dispatch(onOpen(store))
+              }}
+              >
+                <PlusCircle className={'mr-2 h-5 w-5'} />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
           </CommandList>
         </Command>
       </PopoverContent>
